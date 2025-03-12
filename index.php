529d13e145965eb81573c66bb1af1676953dab94
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-12 14:21:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 include 'inc/config.php';
 $message ='';
 session_start();
+
+// Redirect if session is already active
+if (isset($_SESSION['user_id'])) {
+    header('Location: dashboard.php');
+    exit();
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $username = $_POST['username'];
   $password = $_POST['password'];
