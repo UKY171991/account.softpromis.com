9f5392f21d86a0080d52e6a05330b28ed72e5c9f
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-12 15:18:25 +0530

    Update add-income.php

--- add-income.php
+++ add-income.php
@@ -167,11 +167,11 @@ include 'inc/config.php';
         </div>
     </main>
 
-    <script src="assets/js/core/popper.min.js"></script>
+    <!-- <script src="assets/js/core/popper.min.js"></script>
   <script src="assets/js/core/bootstrap.min.js"></script>
   <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
   <script src="assets/js/plugins/smooth-scrollbar.min.js"></script>
-  <script src="assets/js/material-dashboard.min.js?v=3.2.0"></script>
+  <script src="assets/js/material-dashboard.min.js?v=3.2.0"></script> -->
 
     <script>
         document.getElementById("category").addEventListener("change", function () {
