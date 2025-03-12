435d9d57791c8e4031e031a842f4e431e9e2b8a1
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-12 13:48:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -191,7 +191,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <!--   Core JS Files   -->
   <script src="assets/js/core/popper.min.js"></script>
   <script src="assets/js/core/bootstrap.min.js"></script>
-  <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <!-- <script src="assets/js/plugins/perfect-scrollbar.min.js"></script> -->
   <script src="assets/js/plugins/smooth-scrollbar.min.js"></script>
   <script>
     var win = navigator.platform.indexOf('Win') > -1;
