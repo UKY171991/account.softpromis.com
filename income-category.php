a632a10860395150ce6f902994988c31be53a1e8
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-12 14:40:22 +0530

    df

--- income-category.php
+++ income-category.php
@@ -14,37 +14,13 @@
 </head>
 
 <body class="g-sidenav-show bg-gray-100">
-  
-  <!-- Sidebar -->
-  <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-radius-lg fixed-start ms-2 bg-white my-2" id="sidenav-main">
-    <div class="sidenav-header">
-      <i class="fas fa-times p-3 cursor-pointer text-dark opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand px-4 py-3 m-0" href="dashboard.php">
-        <img src="assets/img/logo-ct-dark.png" class="navbar-brand-img" width="26" height="26" alt="main_logo">
-        <span class="ms-1 text-sm text-dark">Creative Tim</span>
-      </a>
-    </div>
-    <hr class="horizontal dark mt-0 mb-2">
-    <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link text-dark" href="dashboard.php">
-            <i class="material-symbols-rounded opacity-5">dashboard</i>
-            <span class="nav-link-text ms-1">Dashboard</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active bg-gradient-dark text-white" href="income-category.php">
-            <i class="material-symbols-rounded opacity-5">category</i>
-            <span class="nav-link-text ms-1">Income Categories</span>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </aside>
+  <?php include 'inc/sidebar.php'; ?>
 
   <!-- Main Content -->
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+
+  	<?php include 'inc/topbar.php'; ?>
+  	
     <div class="container-fluid py-4">
       
       <!-- Page Title -->
