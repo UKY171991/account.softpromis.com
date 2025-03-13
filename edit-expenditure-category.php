65f9d241470de82847204f812e8520b036137140
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-13 13:02:06 +0530

    Update edit-expenditure-category.php

--- edit-expenditure-category.php
+++ edit-expenditure-category.php
@@ -5,7 +5,7 @@ include 'inc/config.php';
 
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
-    $query = "SELECT * FROM income_categories WHERE id = ?";
+    $query = "SELECT * FROM expenditure_categories WHERE id = ?";
     $stmt = mysqli_prepare($conn, $query);
     mysqli_stmt_bind_param($stmt, "i", $id);
     mysqli_stmt_execute($stmt);
