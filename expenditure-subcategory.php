79e3c1200b85a062dfbd8c9c2aadb1040baa3c38
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-13 13:26:50 +0530

    Update expenditure-subcategory.php

--- expenditure-subcategory.php
+++ expenditure-subcategory.php
@@ -9,7 +9,7 @@ include 'inc/config.php';
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Add Income Sub-Category</title>
+  <title>Add Expenditure Sub-Category</title>
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
   <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
   <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
