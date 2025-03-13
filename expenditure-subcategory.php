58195d07105bddfc672b55bc5a23c46dbcc09d73
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-13 13:50:25 +0530

    Update expenditure-subcategory.php

--- expenditure-subcategory.php
+++ expenditure-subcategory.php
@@ -16,6 +16,7 @@ include 'inc/config.php';
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
   <link id="pagestyle" href="assets/css/material-dashboard.css?v=3.2.0" rel="stylesheet" />
+  <link  href="assets/css/style.css" rel="stylesheet" />
 </head>
 
 <body class="g-sidenav-show bg-gray-100">
