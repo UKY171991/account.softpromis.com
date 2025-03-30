6e7cf33d72836a35b4dc4e55f5fb3d82ac04e0c5
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-03-30 16:02:58 +0530

    Update view-income.php

--- view-income.php
+++ view-income.php
@@ -48,6 +48,15 @@ if(isset($_GET['ajax'])) {
     <title>Income Records</title>
     <?php include 'inc/head.php'; ?>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" />
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
+    <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link id="pagestyle" href="assets/css/material-dashboard.css?v=3.2.0" rel="stylesheet" />
+
 </head>
 <body class="bg-gray-100">
     <?php include 'inc/sidebar.php'; ?>
