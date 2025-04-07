60bdd1fb57760e3a011e24dc376ed6ca1a069383
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-04-07 11:38:53 +0530

    Update client.php

--- new/client.php
+++ new/client.php
@@ -72,7 +72,7 @@
     <div class="p-4">
       <div class="d-flex justify-content-between align-items-center mb-4">
         <h5>Clients</h5>
-        <a href="add-client.html" class="btn btn-primary"><i class="bi bi-plus-circle"></i> Add Client</a>
+        <a href="add-client.php" class="btn btn-primary"><i class="bi bi-plus-circle"></i> Add Client</a>
       </div>
 
       <div class="table-responsive">
