d9187e5cd7c34f3e511c26601e85ffdd24c14c68
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-04-08 11:23:33 +0530

    Update income.php

--- new/income.php
+++ new/income.php
@@ -1,14 +1,5 @@
 <?php
-// Database connection
-$host = "localhost";
-$username = "u820431346_new_account";
-$password = "9g/?fYqP+";
-$database = "u820431346_new_account";
 
-$conn = new mysqli($host, $username, $password, $database);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 $sql = "SELECT id, date, name, category, subcategory, amount, received, balance FROM income";
 $result = $conn->query($sql);
