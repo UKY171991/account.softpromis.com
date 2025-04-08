434dccf0ceea711f314b86faf2a3605fe21b7070
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-04-08 11:26:24 +0530

    config

--- new/users.php
+++ new/users.php
@@ -1,14 +1,6 @@
 <?php
 // Database connection
-$host = "localhost";
-$username = "u820431346_new_account";
-$password = "9g/?fYqP+";
-$database = "u820431346_new_account";
-
-$conn = new mysqli($host, $username, $password, $database);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include 'inc/config.php'; // Include the database connection file
 ?>
 
 <!DOCTYPE html>
