346dcc4ffe88133694c0b683178666bfcc5f3622
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-04-09 12:48:12 +0530

    auth

--- new/add-client.php
+++ new/add-client.php
@@ -1,13 +1,7 @@
 <?php
-// Start session
-session_start();
 
-// Check if the user is logged in
-if (!isset($_SESSION['user_id'])) {
-    // Redirect to login page if not logged in
-    header("Location: login.php");
-    exit();
-}
+include 'inc/auth.php'; // Include the database connection file
+
 
 // Database connection
 include 'inc/config.php'; // Include the database connection file
---
+++ new/inc/auth.php
@@ -0,0 +1,11 @@
+<?php
+// Start session
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    // Redirect to login page if not logged in
+    header("Location: login.php");
+    exit();
+}
+?>
