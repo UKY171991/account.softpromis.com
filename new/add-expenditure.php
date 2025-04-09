a9a77ee5201820be15ff877ef4587de5e525361c
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-04-09 14:09:15 +0530

    Update add-expenditure.php

--- new/add-expenditure.php
+++ new/add-expenditure.php
@@ -38,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Add Expenditure</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
   <style>
     body {
       background-color: #f8f9fa;
