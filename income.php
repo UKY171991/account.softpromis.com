d4c0397868cd047b1a33a16b488e5941ee874835
softpro-ppm <softpro.ppm@gmail.com>
2025-05-23 16:35:34 +0530

    Update income.php

--- income.php
+++ income.php
@@ -31,7 +31,7 @@ if (!$result) {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Income Records</title>
+  <title>Income Records 1</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css"/>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css"/>
